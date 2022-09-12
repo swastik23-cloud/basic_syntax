91893306b95a6c263e4d1993aae780b40c1bc39e
Swastik_Shukla <100109150+swastik23-cloud@users.noreply.github.com>
2022-09-12 18:03:19 +0530

    Add files via upload

---
+++ day1_basic_syntax.py
@@ -0,0 +1,17 @@
+# using print function
+from multiprocessing.util import spawnv_passfds
+
+
+print("hello world")
+print (25678)
+
+# using type function
+a=2
+print(type(a))
+b='swastik'
+print(type(b))
+
+# using input function
+a=input("print the number")
+
+print(a)
